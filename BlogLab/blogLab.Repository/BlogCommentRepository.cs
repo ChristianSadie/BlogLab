36ccc3403b198f98b23dc55e2cff1b8579a66c6f
ChristianSadie <70807351+ChristianSadie@users.noreply.github.com>
2021-02-18 10:25:49 +0200

    Completed repositories

---
+++ BlogLab/blogLab.Repository/BlogCommentRepository.cs
@@ -0,0 +1,71 @@
+﻿using BlogLab.Models.BlogComment;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Repository
+{
+    public class BlogCommentRepository : IBlogCommentRepository
+    {
+        private readonly IConfiguration _config;
+
+        public BlogCommentRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task<int> DeleteAsync(int blogCommentId)
+        {
+            int affectedRows = 0;
+
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+
+                affectedRows = await connection.ExecuteAsync("BlogComment_Delete", new { BlogCommentId = blogCommentId }, commandType: CommandType.StoredProcedure);
+            }
+
+            return affectedRows;
+        }
+
+        public async Task<List<BlogComment>> GetAllAsync(int blogId)
+        {
+            IEnumerable<BlogComment> blogComments;
+
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+
+                blogComments = await connection.QueryAsync<BlogComment>("BlogComment_GetAll", new { BlogId = blogId },
+                commandType: CommandType.StoredProcedure
+                   );
+            }
+            return blogComments.ToList();
+        }
+
+        public async Task<BlogComment> GetAsync(int blogCommentId)
+        {
+            BlogComment blogComment;
+
+            using (var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+
+                blogComment = await connection.QueryFirstOrDefaultAsync<Photo>("Photo_Get", new { PhotoId = photoId },
+                commandType: CommandType.StoredProcedure
+                   );
+            }
+            return blogComment;
+        }
+
+        public async Task<BlogComment> Upsert(BlogCommentCreate blogCommentCreate, int applicationUserId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
